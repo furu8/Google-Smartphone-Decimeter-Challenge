f179ff14ea07372975d25acae00c7fa6bd7bc3ab
gacky35 <s21g470@stu.kagawa-u.ac.jp>
2021-06-20 17:17:32 +0900

    ソートとかいろいろ

--- scripts/gacky/checkdata.py
+++ scripts/gacky/checkdata.py
@@ -1,4 +1,6 @@
-#%%
+# %%
+## 各種インポート
+from re import sub
 import numpy as np
 from numpy.lib.function_base import disp
 import pandas as pd
@@ -7,87 +9,6 @@ from IPython.core.display import display, display_pdf
 import glob as gb
 import matplotlib.pyplot as plt
 import seaborn as sns
-# %%
-## データ読み込み
-submission = pd.read_csv('../../data/submission/sample_submission.csv')
-baseline_train = pd.read_csv('../../data/raw/baseline_locations_train.csv')
-raw_data = pd.read_csv('../../data/raw/train/2020-05-29-US-MTV-1/Pixel4XL/Pixel4XL_derived.csv')
-raw_data_test = pd.read_csv('../../data/raw/test/2020-05-28-US-MTV-1/Pixel4XL/Pixel4XL_derived.csv')
-gt = pd.read_csv('../../data/raw/train/2020-05-29-US-MTV-1/Pixel4XL/ground_truth.csv')
-# %%
-display(raw_data.shape)
-display(gt.shape)
-# %%
-display(raw_data)
-display(gt)
-# %%
-raw_data['millisSinceGpsEpoch'].plot()
-# %%
-gt['millisSinceGpsEpoch'].plot()
-# %%
-display(raw_data.groupby('millisSinceGpsEpoch').mean())
-# %%
-## merge_asofで結合マン
-merge_df = pd.merge_asof(raw_data, gt, on="millisSinceGpsEpoch", by=["collectionName", "phoneName"], direction="nearest", allow_exact_matches=True)
-display(merge_df)
-# %%
-display(merge_df.describe())
-display(merge_df.info())
-# %%
-display(raw_data.dtypes)
-display(raw_data_test.dtypes)
-display(gt.dtypes)
-# %%
-!pip install pynmea2
-# %%
-import pynmea2
-# %%
-## nmeaをdataframeにしよう
-with open("../../data/raw/train/2020-05-14-US-MTV-1/Pixel4/supplemental/SPAN_Pixel4_10Hz.nmea", encoding='utf-8') as nmea_f:
-    for line in nmea_f.readlines():
-        try:
-            msg = pynmea2.parse(line)
-            break
-        except pynmea2.ParseError as e:
-            pass
-        
-print(repr(msg))
-print("---------------------------------")
-print("timestamp:", msg.timestamp)
-print("lat:", msg.lat)
-print("lat_dir:", msg.lat_dir)
-print("lon:", msg.lon)
-print("lon_dir:", msg.lon_dir)
-print("gps_qual:", msg.gps_qual)
-print("num_sats:", msg.num_sats)
-print("horizontal_dil:", msg.horizontal_dil)
-print("altitude:", msg.altitude)
-print("altitude_units:", msg.altitude_units)
-print("geo_sep:", msg.geo_sep)
-print("geo_sep_units:", msg.geo_sep_units)
-print("age_gps_data:", msg.age_gps_data)
-print("ref_station_id:", msg.ref_station_id)
-print("---------------------------------")
-# %%
-def load_nmea_file(nmea_path):
-    """Convert GGA sentences of nmea files to pandas dataframe.
-    """
-    
-    gga_lines = []
-    with open(nmea_path, encoding='utf-8') as nmea_f:
-        for line in nmea_f.readlines():
-            try:
-                msg = pynmea2.parse(line)
-                gga_lines.append(msg)
-            except pynmea2.ParseError as e:
-                pass
-            
-      
-    return pd.DataFrame(gga_lines)
-# %%
-nmea_sample = load_nmea_file("../../data/raw/train/2020-05-14-US-MTV-1/Pixel4/supplemental/SPAN_Pixel4_10Hz.nmea")
-# %%
-display(nmea_sample)
 # %%
 # GNSSLogのテキストをdataframeにしよう
 def gnss_log_to_dataframes(path):
@@ -105,7 +26,10 @@ def gnss_log_to_dataframes(path):
         if is_header and dataline[0] in gnss_section_names:
             gnss_map[dataline[0]] = dataline[1:]
         elif not is_header:
-            datas[dataline[0]].append(dataline[1:])
+            try:
+                datas[dataline[0]].append(dataline[1:])
+            except KeyError:
+                print(dataline[0])
 
     results = dict()
     for k, v in datas.items():
@@ -115,40 +39,17 @@ def gnss_log_to_dataframes(path):
         for col in df.columns:
             if col == 'CodeType':
                 continue
-            results[k][col] = pd.to_numeric(results[k][col])
+            try:
+                results[k][col] = pd.to_numeric(results[k][col])
+                if col == 'utcTimeMillis' or col == 'UnixTimeMillis':
+                    results[k]['millisSinceGpsEpoch'] = results[k][col].apply(unix2gps)
+                    results[k].drop(col, axis=1, inplace=True)
+            except ValueError:
+                results[k] = results[k][:-1]
+                results[k][col] = pd.to_numeric(results[k][col])
+    #results["Raw"]["rawPrM"] = results["Raw"]["TimeNanos"] - results["Raw"]["FullBiasNanos"] - results["Raw"]["BiasNanos"]
 
     return results
-# %%
-## 決まった分を日付限定してやっていきましょう
-data_path = "../../data/raw/train/2020-05-14-US-MTV-1/Pixel4/"
-beta_tr_p4 = pd.read_csv(data_path + "Pixel4_derived.csv")
-beta_gt_p4 = pd.read_csv(data_path + "ground_truth.csv")
-# %%
-beta_p4_merge = pd.merge_asof(beta_tr_p4, beta_gt_p4, on="millisSinceGpsEpoch", by=["collectionName", "phoneName"], direction="nearest")
-# %%
-beta_p4_merge = beta_p4_merge.drop(["heightAboveWgs84EllipsoidM", "timeSinceFirstFixSeconds", "hDop", "vDop", "speedMps", "courseDegree"], axis=1)
-# %%
-len(beta_p4_merge["millisSinceGpsEpoch"].unique())
-# %%
-beta_p4_merge.info()
-# %%
-dfs = gnss_log_to_dataframes(data_path + 'Pixel4_GnssLog.txt')
-# %%
-dfs.keys()
-# %%
-sample_df = pd.read_csv("../../data/raw/train/2020-05-14-US-MTV-1/Pixel4/Pixel4_derived.csv")
-# %%
-display(sample_df.shape)
-# %%
-dfs["UncalGyro"].groupby('utcTimeMillis').mean()
-# %%
-beta_p4_UncalAccel = dfs["UncalAccel"]
-# %%
-len(beta_p4_UncalAccel["utcTimeMillis"].unique())
-# %%
-beta_p4_UncalAccel["utcTimeMillis"].plot()
-# %%
-beta_p4_UncalAccel.info()
 # %%
 ## あっとるんかわからんけどUTCとGPSを変換するらしいコードをパクるよ(PHP製)
 from math import fmod
@@ -159,52 +60,69 @@ def unix2gps(unix_time):
     else:
         isleap = 0
     gps_time = unix_time - 315964800000
-    nleaps = countleaps(gps_time, 'unix2gps')
+    nleaps = countleaps(gps_time)
     gps_time = gps_time + nleaps + isleap
     return gps_time
 
-def countleaps(gps_time, dirFlag):
+def countleaps(gps_time):
     leaps = getleaps()
     lenleaps = len(leaps)
     nleaps = 0
     for i in range(lenleaps):
-        if dirFlag != 'unix2gps':
-            if gps_time >= leaps[i] - i:
-                nleaps += 1
+        if gps_time >= leaps[i] - i:
+            nleaps += 1000
     return nleaps
 
 def getleaps():
-    leaps = [46828800, 78364801, 109900802, 173059203, 252028804, 315187205, 346723206, 393984007, 425520008, 457056009, 504489610, 551750411, 599184012, 820108813, 914803214, 1025136015, 1119744016, 1167264017]
+    leaps = [46828800000, 78364801000, 109900802000, 173059203000, 252028804000, 315187205000, 346723206000, 393984007000, 425520008000, 457056009000, 504489610000, 551750411000, 599184012000, 820108813000, 914803214000, 1025136015000, 1119744016000, 1167264017000]
     return leaps
 # %%
-beta_p4_UncalAccel["utcTimeMillis"].apply(unix2gps)
+## GnssLogのデータフレームたちを一つにまとめます
+def merge_gnss_data(gnss_data):
+    gnss_section_names = ["Raw", "Status", "UncalAccel", "UncalGyro", "UncalMag", "Fix", "OrientationDeg"]
+    gnss_data["Raw"]["elapsedRealtimeNanos"] = np.nan
+    for sub_data in gnss_section_names:
+        try:
+            merge_data = pd.merge_asof(merge_data, gnss_data[sub_data], on="millisSinceGpsEpoch", direction="nearest", tolerance=1000, allow_exact_matches=True, suffixes=('', sub_data))
+        except UnboundLocalError:
+            merge_data = gnss_data[sub_data]
+        except KeyError:
+            print(f'{sub_data} is not in this log data.')
+    merge_data.drop("elapsedRealtimeNanos", axis=1, inplace=True)
+    #display(merge_data)
+    return merge_data
 # %%
-beta_p4_merge["millisSinceGpsEpoch"].head(50)
+## ソート問題を何とかする関数
+def sort_gnss_dataframe(gnss_dict):
+    for sub_name in gnss_dict.keys():
+        try:
+            gnss_dict[sub_name]["Index"] = gnss_dict[sub_name].index
+            gnss_dict[sub_name] = gnss_dict[sub_name].sort_values(["millisSinceGpsEpoch", "Index"], ignore_index=True)
+            gnss_dict[sub_name].drop("Index", axis=1, inplace=True)
+        except KeyError:
+            print(f"{sub_name} is not have millisSinceGpsEpoch")
+    return gnss_dict
 # %%
-beta_p4_UncalAccel["utcTimeMillis"].plot()
+df_dict = gnss_log_to_dataframes("../../data/raw/train/2021-01-05-US-SVL-1/Pixel5/Pixel5_GnssLog.txt")
+derived_data = pd.read_csv("../../data/raw/train/2021-01-05-US-SVL-1/Pixel5/Pixel5_derived.csv")
+gt = pd.read_csv("../../data/raw/train/2021-01-05-US-SVL-1/Pixel5/ground_truth.csv")
 # %%
-dfs["UncalGyro"]["utcTimeMillis"].plot()
-dfs["UncalGyro"].info()
+df_dict['Raw'].info()
+df_dict['Status'].info()
+len(set(df_dict['Raw']["millisSinceGpsEpoch"].unique()) - set(df_dict['Status']["millisSinceGpsEpoch"].unique()))
 # %%
-dfs["UncalMag"]["utcTimeMillis"].plot()
-dfs["UncalMag"].info()
+dfs = merge_gnss_data(df_dict)
+display(dfs.info())
+merge_data = pd.merge_asof(pd.merge_asof(dfs, derived_data, on="millisSinceGpsEpoch", direction="nearest", tolerance=1000, allow_exact_matches=True), gt, on="millisSinceGpsEpoch", direction="nearest", tolerance=1000, allow_exact_matches=True)
+merge_data.describe()
 # %%
-dfs["Status"]["UnixTimeMillis"].plot()
-dfs["Status"].info()
+for i in merge_data.columns:
+    print(i)
+
 # %%
-dfs["Raw"]["utcTimeMillis"].plot()
-dfs["Raw"].info()
-# %%
-dfs["Fix"]["UnixTimeMillis"].plot()
-# %%
-alpha_p4_tr = pd.read_csv("../../data/raw/train/2020-05-14-US-MTV-2/Pixel4/Pixel4_derived.csv")
-alpha_p4_gt = pd.read_csv("../../data/raw/train/2020-05-14-US-MTV-2/Pixel4/ground_truth.csv")
-# %%
-theta_p4_tr = pd.concat([beta_tr_p4, alpha_p4_tr], ignore_index=True)
-theta_p4_gt = pd.concat([beta_gt_p4, alpha_p4_gt], ignore_index=True)
-theta_p4_merge = pd.merge_asof(theta_p4_tr, theta_p4_gt, on="millisSinceGpsEpoch", by=["collectionName", "phoneName"], direction="nearest", tolerance=100000)
-# %%
-theta_p4_merge
+for i, value in merge_data.isnull().sum().iteritems():
+    if value > 0:
+        print(i, value)
 # %%
 ## 結合するためのコードを書きます
 ## とりあえず端末ごとに日毎の取得データとground_truthをくっつけますね
@@ -218,19 +136,147 @@ for root, dirs, files in os.walk(base_path):
     gt = None
     get_data = None
     for csv in csvs:
-        if gt is None:
-            gt = pd.read_csv(os.path.join(root, csv))
-        elif get_data is None:
+        if 'Mi8' not in root:
+            continue
+        else:
+            if gt is None:
+                gt = pd.read_csv(os.path.join(root, csv))
+            elif get_data is None:
+                get_data = pd.read_csv(os.path.join(root, csv))
+                get_data.rename(columns={"svid": "Svid", "constellationType": "ConstellationType"}, inplace=True)
+            if gt is not None and get_data is not None:
+                try:
+                    gnss_data = gnss_log_to_dataframes(os.path.join(root, csv).replace("derived.csv", "GnssLog.txt"))
+                    merge_data = pd.merge_asof(pd.merge_asof(merge_gnss_data(sort_gnss_dataframe(gnss_data)), get_data, on="millisSinceGpsEpoch", direction="nearest", tolerance=100000, allow_exact_matches=True), gt, on="millisSinceGpsEpoch", by=["collectionName", "phoneName"], direction="nearest", tolerance=100000, allow_exact_matches=True)
+                    #merge_data_2 = pd.merge_asof(merge_gnss_data(sort_gnss_dataframe(gnss_data)), get_data, on="millisSinceGpsEpoch", by=["Svid", "ConstellationType"], direction="nearest", tolerance=100000, allow_exact_matches=True)
+                    #merge_data = pd.merge_asof(merge_data_2, gt, on="millisSinceGpsEpoch", by=["collectionName", "phoneName"], direction="nearest", tolerance=100000, allow_exact_matches=True)
+                    dfs[os.path.join(root, csv).split('\\')[1]] = pd.concat([dfs[os.path.join(root, csv).split('\\')[1]], merge_data])
+                    #dfs[os.path.join(root, csv).split('\\')[1] + "_2"] = pd.concat([dfs[os.path.join(root, csv).split('\\')[1] + "_2"], merge_data_2])
+                except KeyError:
+                    dfs[os.path.join(root, csv).split('\\')[1]] = merge_data
+                    #dfs[os.path.join(root, csv).split('\\')[1] + "_2"] = merge_data_2
+            #except pd.errors.InvalidIndexError:
+            #    display(dfs[os.path.join(root, csv).split('\\')[1]])
+            #    display(merge_data)
+                gt = get_data = None
+# %%
+dfs["Pixel4"].to_csv("../../data/interim/merged_test_Pixel4.csv", sep=",")
+dfs["Pixel4XL"].to_csv("../../data/interim/merged_test_Pixel4XL.csv", sep=",")
+dfs["Pixel4Modded"].to_csv("../../data/interim/merged_test_Pixel4Modded.csv", sep=",")
+dfs["Pixel4XLModded"].to_csv("../../data/interim/merged_test_Pixel4XLModded.csv", sep=",")
+dfs["Pixel5"].to_csv("../../data/interim/merged_test_Pixel5.csv", sep=",")
+dfs["Mi8"].to_csv("../../data/interim/merged_test_Mi8.csv", sep=",")
+dfs["SamsungS20Ultra"].to_csv("../../data/interim/merged_test_SamsungS20Ultra.csv", sep=",")
+# %%
+for col in dfs["Mi8_2"].columns:
+    print(col)
+# %%
+dfs["Mi8_2"]["collectionName"]#[dfs["Mi8"]["collectionName"] == "2021-01-15-US-SVL-1"]
+# %%
+def check_sort(gnss_dict):
+    for sub_name, gnss_datum in gnss_dict.items():
+        #print(sub_name, gnss_datum)
+        #for col in gnss_datum.columns:
+        try:
+            if len(gnss_dict[sub_name][gnss_dict[sub_name]["millisSinceGpsEpoch"].diff()<0]) > 0:
+                print(sub_name)
+                display(gnss_dict[sub_name][gnss_dict[sub_name]["millisSinceGpsEpoch"].diff()<0])
+        except KeyError:
+            print(f'{sub_name} is not have millisSinceGpsEpoch')
+# %%
+## なんか順番おかしいのがあるらしいんで探そ
+import os
+phone_list = ["Pixel4", "Pixel4XL", "Pixel4Modded", "Pixel4XLModded", "Mi8", "SamsungS20Ultra", "Pixel5"]
+base_path = "../../data/raw/train/"
+df = pd.DataFrame
+for root, dirs, files in os.walk(base_path):
+    gnss_logs = filter(lambda f: f.endswith("Pixel4_GnssLog.txt"), files)
+    #gt = None
+    #get_data = None
+    for gnss_log in gnss_logs:
+        print(os.path.join(root, gnss_log))
+        #if gt is None:
+        #    gt = pd.read_csv(os.path.join(root, csv))
+        #elif get_data is None:
+        #    get_data = pd.read_csv(os.path.join(root, csv))
+        #if gt is not None and get_data is not None:
+        #gnss_dict = gnss_log_to_dataframes(os.path.join(root, gnss_log))
+        #check_sort(gnss_dict)
+# %%
+gnss_dict = gnss_log_to_dataframes("../../data/raw/train/2020-07-17-US-MTV-2/Mi8/Mi8_GnssLog.txt")
+# %%
+check_sort(gnss_dict)
+# %%
+len(gnss_dict['UncalGyro'][3800:3802])
+# %%
+## Pixel4の結合をなんとかしなきゃ
+gnss_dict_Pixel4_1 = gnss_log_to_dataframes("../../data/raw/train/2020-05-14-US-MTV-1/Pixel4/Pixel4_GnssLog.txt")
+# %%
+check_sort(gnss_dict_Pixel4_1)
+# %%
+gnss_dict_Pixel4_1["Status"]["Index"] = gnss_dict_Pixel4_1["Status"].index
+gnss_dict_Pixel4_1["Status"] = gnss_dict_Pixel4_1["Status"].sort_values(["millisSinceGpsEpoch", "Index"], ignore_index=True)
+check_sort(gnss_dict_Pixel4_1)
+# %%
+gnss_dict_Pixel4_1["Status"].drop("Index", axis=1, inplace=True)
+# %%
+for col in gnss_dict_Pixel4_1["Raw"].columns:
+    print(col)
+# %%
+gnss_dict_Pixel4_1["Raw"][["Svid", "ConstellationType", "millisSinceGpsEpoch"]]
+# %%
+gnss_dict_Pixel4_1["UncalGyro"]["Index"] = gnss_dict_Pixel4_1["UncalGyro"].index
+gnss_dict_Pixel4_1["UncalGyro"] = gnss_dict_Pixel4_1["UncalGyro"].sort_values(["millisSinceGpsEpoch", "Index"], ignore_index=True)
+gnss_dict_Pixel4_1["UncalAccel"]["Index"] = gnss_dict_Pixel4_1["UncalAccel"].index
+gnss_dict_Pixel4_1["UncalAccel"] = gnss_dict_Pixel4_1["UncalAccel"].sort_values(["millisSinceGpsEpoch", "Index"], ignore_index=True)
+check_sort(gnss_dict_Pixel4_1)
+# %%
+gnss_dict_Pixel4_1["UncalAccel"][68305:68315]
+# %%
+Mi8_df = pd.read_csv('../../data/interim/merged_Mi8.csv', index_col=0)
+# %%
+Mi8_df.head()
+# %%
+def check_df_sort(gnss_df):
+    if len(gnss_df[gnss_df["millisSinceGpsEpoch"].diff()<0]) > 0:
+        display(gnss_df[gnss_df["millisSinceGpsEpoch"].diff()<0])
+    else:
+        print("No Problem.")
+
+# %%
+check_df_sort(Mi8_df)
+# %%
+Pixel4_df = pd.read_csv('../../data/interim/merged_Pixel4.csv', index_col=0)
+# %%
+check_df_sort(Pixel4_df)
+# %%
+Pixel4_df[Pixel4_df["collectionName"] == "2021-04-29-US-SJC-2"][["receivedSvTimeInGpsNanos", "millisSinceGpsEpoch"]].unique()
+
+# %%
+Pixel4_df["SignalIndex"]
+# %%
+phone_list = ["Pixel4", "Pixel4XL", "Pixel4Modded", "Pixel4XLModded", "Mi8", "SamsungS20Ultra", "Pixel5"]
+base_path = "../../data/raw/test/"
+dfs = dict()
+import os
+for root, dirs, files in os.walk(base_path):
+    csvs = filter(lambda f: f.endswith(".csv"), files)
+    get_data = None
+    for csv in csvs:
+        if get_data is None:
             get_data = pd.read_csv(os.path.join(root, csv))
+            get_data.rename(columns={"svid": "Svid", "constellationType": "ConstellationType"}, inplace=True)
         if gt is not None and get_data is not None:
             try:
-                merge_data = pd.merge_asof(get_data, gt, on="millisSinceGpsEpoch", by=["collectionName", "phoneName"], direction="nearest", tolerance=100000, allow_exact_matches=True)
-                dfs[os.path.join(root, csv).split('\\')[1]] = pd.concat([dfs[os.path.join(root, csv).split('\\')[1]], merge_data], ignore_index=True)
+                gnss_data = gnss_log_to_dataframes(os.path.join(root, csv).replace("derived.csv", "GnssLog.txt"))
+                merge_data = pd.merge_asof(pd.merge_asof(merge_gnss_data(sort_gnss_dataframe(gnss_data)), get_data, on="millisSinceGpsEpoch", direction="nearest", tolerance=100000, allow_exact_matches=True), gt, on="millisSinceGpsEpoch", by=["collectionName", "phoneName"], direction="nearest", tolerance=100000, allow_exact_matches=True)
+                dfs[os.path.join(root, csv).split('\\')[1]] = pd.concat([dfs[os.path.join(root, csv).split('\\')[1]], merge_data])
             except KeyError:
                 dfs[os.path.join(root, csv).split('\\')[1]] = merge_data
-            gt = get_data = None
+            get_data = None
 # %%
-dfs.keys()
+s = -7.0277543E-4
+print(f"{s:f}")
 # %%
-dfs["Pixel4"]
+gnss_log_to_dataframes("../../data/raw/test/2021-04-21-US-MTV-1/Pixel4Modded/Pixel4Modded_GnssLog.txt")
 # %%
