2f4148d56f961901ab06f730e71b2d5dde26dc61
furu8 <foooolurk1125@gmail.com>
2021-07-20 03:20:02 +0900

    feat:外れ値imu

--- scripts/furu/analyze_outlier.py
+++ scripts/furu/analyze_outlier.py
@@ -19,14 +19,8 @@ pd.set_option('display.max_columns', 500)
 
 # %%
 class Outlier:
-    def __init__(self, phone_name) -> None:
-        self.phone_name = phone_name
-        self.df = None
-        self.plot_df = None
-
-    def load_df(self, base_path, dirname, _filename):
-        self.df = pd.read_csv(f'{base_path}/{dirname}/merged_{self.phone_name}_{_filename}.csv')
-        self.plot_df = pd.read_csv(f'{base_path}/{dirname}/merged_{self.phone_name}_{_filename}.csv')
+    def __init__(self, df) -> None:
+        self.df = df
 
     def detect_outlier(self, col):
         df = self.df.select_dtypes(include='number').copy() # int, floatだけ抽出
@@ -37,31 +31,86 @@ class Outlier:
 
     def plot_hist(self, col):
         plt.figure(figsize=(4,3))
-        self.plot_df[col].hist()
+        self.df[col].hist()
         plt.show()
 
     def plot_onedate_hist(self, col, date):
-        plot_df = self.plot_df[self.plot_df['collectionName']==date]
+        plot_df = self.df[self.df['collectionName']==date]
         plt.figure(figsize=(4,3))
         plot_df[col].hist()
         plt.show()
 
     def plot_onedate_line(self, col, date):
-        plot_df = self.plot_df[self.plot_df['collectionName']==date]
+        plot_df = self.df[self.df['collectionName']==date]
         plt.figure(figsize=(20,4))
         plot_df[col].plot()
         plt.show()
 
 # %%
-%%time
-phone_name = input('スマホの名前指定: ')
-dir_name = input('ディレクトリの名前指定（train/test）: ')
-file_name = input('ファイルの名前指定（derived/gt等）: ')
+def prepare_imu_data(dataset_name, cname, pname):
+    '''Prepare IMU Dataset (For Train: IMU+GT+BL; For Test: IMU+BL)
+    Input：
+        1. data_dir: data_dir
+        2. dataset_name: dataset name（'train'/'test'）
+        3. cname: CollectionName
+        4. pname: phoneName
+        5. bl_df: baseline's dataframe
+    Output：df_all
+    '''
+    # load GNSS log
+    acc_df = pd.read_csv(f'../../data/interim/{dataset_name}/merged_{pname}_UncalAccel_add_columns.csv')
+    gyr_df = pd.read_csv(f'../../data/interim/{dataset_name}/merged_{pname}_UncalGyro_add_columns.csv')
+    mag_df = pd.read_csv(f'../../data/interim/{dataset_name}/merged_{pname}_UncalMag_add_columns.csv')
+    ort_df = pd.read_csv(f'../../data/interim/{dataset_name}/merged_{pname}_OrientationDeg_add_columns.csv')
 
-basepath = '../../data/interim'
-outlier = Outlier(phone_name)
-outlier.load_df(basepath, dir_name, file_name)
-outlier.df
+    # acc_df = acc_df[acc_df['collectionName']==cname].reset_index(drop=True)
+    # gyr_df = gyr_df[gyr_df['collectionName']==cname].reset_index(drop=True)
+    # mag_df = mag_df[mag_df['collectionName']==cname].reset_index(drop=True)
+    # ort_df = ort_df[ort_df['collectionName']==cname].reset_index(drop=True)
+
+    acc_df['millisSinceGpsEpoch'] = acc_df['millisSinceGpsEpoch'].astype(np.int64)
+    gyr_df['millisSinceGpsEpoch'] = gyr_df['millisSinceGpsEpoch'].astype(np.int64)
+    mag_df['millisSinceGpsEpoch'] = mag_df['millisSinceGpsEpoch'].astype(np.int64)
+    ort_df['millisSinceGpsEpoch'] = ort_df['millisSinceGpsEpoch'].astype(np.int64)
+
+    # display(acc_df)
+    # display(gyr_df)
+    # display(mag_df)
+    # display(ort_df)
+
+    # merge sub-datasets
+    # accel + gyro
+    imu_df = pd.merge_asof(acc_df.sort_values('millisSinceGpsEpoch'),
+                           gyr_df.drop('elapsedRealtimeNanos', axis=1).sort_values('millisSinceGpsEpoch'),
+                           on = 'millisSinceGpsEpoch',
+                           by=['collectionName', 'phoneName'],
+                           direction='nearest')
+    # (accel + gyro) + mag
+    imu_df = pd.merge_asof(imu_df.sort_values('millisSinceGpsEpoch'),
+                           mag_df.drop('elapsedRealtimeNanos', axis=1).sort_values('millisSinceGpsEpoch'),
+                           on = 'millisSinceGpsEpoch',
+                           by=['collectionName', 'phoneName'],
+                           direction='nearest')
+    # ((accel + gyro) + mag) + OrientationDeg
+    imu_df = pd.merge_asof(imu_df.sort_values('millisSinceGpsEpoch'),
+                           ort_df.drop('elapsedRealtimeNanos', axis=1).sort_values('millisSinceGpsEpoch'),
+                           on = 'millisSinceGpsEpoch',
+                           by=['collectionName', 'phoneName'],
+                           direction='nearest')
+
+    return imu_df
+# %%
+%%time
+dir_name = 'train'
+collection_name = '2021-04-22-US-SJC-1'
+phone_name = 'SamsungS20Ultra'
+
+imu_df = prepare_imu_data(dir_name, collection_name, phone_name)
+imu_df
+
+# %%
+outlier = Outlier(imu_df)
+
 # %%
 outlier.df.describe()
 
@@ -69,12 +118,7 @@ outlier.df.describe()
 outlier.df.info()
 
 # %%
-# Mi8_train_Status_add_columnsのmillisSinceGpsEpochが、取り始めにアンダフロー値が出てる
-# outlier.df[['millisSinceGpsEpoch', 'collectionName']][2700:2850]
-# outlier.df[['millisSinceGpsEpoch', 'collectionName']][50300:50340]
-# outlier.df.loc[outlier.df['millisSinceGpsEpoch']<0, ['millisSinceGpsEpoch', 'collectionName']]
-
-# %%
+%%time
 # 外れ値
 outlier_col_list = []
 outlier_date_dict = {}
@@ -110,6 +154,13 @@ for col, date_list in outlier_date_dict.items():
 
 # %%
 %%time
+# 全体
+for col in imu_df.columns:
+    print(col)
+    plt.hist(imu_df[col])
+    plt.show()
+# %%
+%%time
 # 外れ値だけでヒストグラム可視化（全体）
 print(outlier_col_list)
 for col in outlier_col_list:
@@ -124,6 +175,10 @@ for col, date_list in outlier_date_dict.items():
     for date in date_list:
         print(col, date)
         outlier.plot_onedate_line(col, date=date)
+
+# %%
+# kalman
+
 # %%
 # profile = pdp.ProfileReport(train_dr_df)
 # profile.to_file(outputfile=f'train_dr_df_{phone_name}.html')
